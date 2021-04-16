30d87a7b01965b635a0a8bcd75373f5a7892c7a9
mooroom <61869246+mooroom@users.noreply.github.com>
2021-04-16 18:19:51 +0900

    split function

--- test.cpp
+++ test.cpp
@@ -7,7 +7,57 @@
 #include <iostream>
 #include <string>
 #include <ctime>
+#include <sstream>
 
+#include <vector>
 #include <pwd.h>
 
 using namespace std;
+
+vector<string> split(string);
+
+int main()
+{
+    // const char *s1 = "hello1";
+    // char s2[] = "hello2";
+    // const char *s3[] = {"hello3"};
+
+    // string s4 = "hello4";
+    // const char *s5 = s4.c_str();
+
+    // int x1[] = {1, 2, 3, 4};
+
+    // cout << s1 << endl;
+    // cout << s2 << endl;
+    // cout << s3[0] << endl;
+    // cout << s4 << endl;
+    // cout << s5 << endl;
+    vector<string> strvec;
+    string input;
+    cout << "input: ";
+    getline(cin, input);
+    strvec = split(input);
+
+    // for (int i = 0; i < strvec.size(); i++)
+    // {
+    //     cout << strvec[i] << endl;
+    // }
+    string last_cmd = strvec.back();
+    if (last_cmd == "&")
+        cout << "is background" << endl;
+
+    return 0;
+}
+
+vector<string> split(string line)
+{
+    vector<string> v;
+    istringstream ss(line);
+    string buff;
+    while (getline(ss, buff, ' '))
+    {
+        v.push_back(buff);
+    }
+
+    return v;
+}
